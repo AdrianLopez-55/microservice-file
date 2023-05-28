5205042beb2db0d1d4fc283c6a81ccfb73100bfc
Eric Rojo <email@gmail.com>
2023-05-27 20:00:01 -0400

    Limit size added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as multer from 'multer';
 import { AllExceptionsFilter } from './all-exceptions.filter';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({ limit: '5mb' }));
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
 }
