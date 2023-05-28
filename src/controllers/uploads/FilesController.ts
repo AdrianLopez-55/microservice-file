759de0255fd201bb86ac1eb90e027230b6f3bb93
Eric Rojo <email@gmail.com>
2023-05-27 20:12:46 -0400

    Types JSON Mime Format Ok

--- src/controllers/uploads/FilesController.ts
+++ src/controllers/uploads/FilesController.ts
@@ -7,25 +7,15 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('upload')
-  async uploadFile(@Body() body: { file: string }) {
-    const base64Data = body.file;
-
-    // Obtener el tipo de contenido y los datos del archivo desde la cadena Base64
-    const matches = base64Data.match(/^data:([A-Za-z-+/]+);base64,(.+)$/);
-    if (matches.length !== 3) {
-      throw new Error('Cadena Base64 no válida');
-    }
-
-    const contentType = matches[1];
-    const fileData = matches[2];
-    const fileBuffer = Buffer.from(fileData, 'base64');
+  async uploadFile(@Body() body: { file: { mime: string, data: string } }) {
+    const fileData = body.file;
 
     // Generar un nombre de archivo único utilizando UUID y mantener la extensión original
-    const filename = `${uuidv4()}.${contentType.split('/')[1]}`;
+    const filename = `${uuidv4()}.${fileData.mime.split('/')[1]}`;
 
     // Llamar al método del servicio para guardar el archivo
-    const savedFile = await this.filesService.saveFile(filename, fileBuffer);
+    await this.filesService.saveFile(filename, fileData);
 
-    return { message: 'Archivo guardado correctamente', savedFile };
+    return { message: 'Archivo guardado correctamente' };
   }
 }
--- src/services/uploads/files.services.ts
+++ src/services/uploads/files.services.ts
@@ -3,8 +3,7 @@ import { writeFileSync, existsSync, mkdirSync } from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { File,FileDocument } from 'src/schemas/files.schema';
-
+import { File, FileDocument } from 'src/schemas/files.schema';
 
 @Injectable()
 export class FilesService {
@@ -12,40 +11,32 @@ export class FilesService {
     @InjectModel(File.name) private fileModel: Model<FileDocument>,
   ) {}
 
-  async saveFile(filename: string,  fileBuffer: Buffer) {
-    const directory = 'uploads'; // Ruta de la carpeta de destino
+  async saveFile(filename: string, fileData: { mime: string, data: string }) {
+    const directory = 'uploads';
 
-    // Verificar si el directorio existe, si no, crearlo
     if (!existsSync(directory)) {
       mkdirSync(directory);
     }
-    
-    // Obtener la extensión del archivo original
-    const fileExtension = filename.split('.').pop();
 
-
-    // Generar un nombre de archivo único utilizando UUID
+    const fileExtension = fileData.mime.split('/')[1];
     const uniqueFilename = `${uuidv4()}.${fileExtension}`;
-
-    // Resto del código para guardar el archivo en el directorio
     const filePath = `${directory}/${uniqueFilename}`;
+    const fileBuffer = Buffer.from(fileData.data, 'base64');
+
     writeFileSync(filePath, fileBuffer);
 
     console.log(`Archivo "${uniqueFilename}" guardado correctamente en "${filePath}"`);
 
-    // Puedes realizar otras operaciones o retornar alguna respuesta aquí si es necesario
-      // Guardar los datos del archivo en la base de datos
-      const file = new this.fileModel({
-        filename,
-        originalname:uniqueFilename,
-        extension: fileExtension,
-        size: fileBuffer.length,
-        filePath,
-      });
-  
-      await file.save();
-  
-      console.log('Datos del archivo guardados en la base de datos:', file);
-  
+    const file = new this.fileModel({
+      filename,
+      originalname: uniqueFilename,
+      extension: fileExtension,
+      size: fileBuffer.length,
+      filePath,
+    });
+
+    await file.save();
+
+    console.log('Datos del archivo guardados en la base de datos:', file);
   }
 }
