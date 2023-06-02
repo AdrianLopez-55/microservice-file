388017c2cae5887e40725189f4a1a4be525f3f85
unknown <Acer Aspire>
2023-06-02 16:00:07 -0400

    Files Saving Corrected

--- src/services/uploads/files.services.ts
+++ src/services/uploads/files.services.ts
@@ -8,58 +8,54 @@ import { File, FileDocument, FileCategory } from 'src/schemas/files.schema';
 @Injectable()
 export class FilesService {
   private readonly logger = new Logger(FilesService.name);
+
   constructor(
     @InjectModel(File.name) private fileModel: Model<FileDocument>,
   ) {}
 
   async saveFile(filename: string, fileExtension: string, fileData: string) {
-    try{
+    try {
       const now = new Date();
-    const year = now.getFullYear().toString();
-    const month = now.toLocaleString('default', { month: 'long' });
-    const day = now.getDate().toString();
-    const dayOfWeek = now.toLocaleString('default', { weekday: 'long' });
-    const directory = `/home/dby823/FilesBackend/${year}/${month}/${day}_${dayOfWeek}`; // Ruta de la carpeta de destino
+      const year = now.getFullYear().toString();
+      const month = now.toLocaleString('default', { month: 'long' });
+      const day = now.getDate().toString();
+      const dayOfWeek = now.toLocaleString('default', { weekday: 'long' });
+      const directory = `uploads/${year}/${month}/${day}_${dayOfWeek}`;
 
-    // Verificar si el directorio existe, si no, crearlo
-    if (!existsSync(directory)) {
-      mkdirSync(directory, { recursive: true });
-    }
+      // Verificar si el directorio existe, si no, crearlo
+      if (!existsSync(directory)) {
+        mkdirSync(directory, { recursive: true });
+      }
 
-    // Generar un nombre de archivo único utilizando UUID
-    const uniqueFilename = `${uuidv4()}.${fileExtension}`;
+      const uniqueFilename = `${uuidv4()}.${fileExtension}`;
+      const filePath = `${directory}/${uniqueFilename}`;
+      writeFileSync(filePath, fileData, { encoding: 'base64' });
 
-    // Resto del código para guardar el archivo en el directorio
-    const filePath = `${directory}/${uniqueFilename}`;
-    writeFileSync(filePath, fileData, { encoding: 'base64' });
- 
-    // Determinar la categoría según la extensión del archivo
-    let category: FileCategory;
+      let category: FileCategory;
 
-    if (['jpg', 'jpeg', 'png', 'gif', 'bmp'].includes(fileExtension.toLowerCase())) {
-      category = FileCategory.Image;
-    } else if (['doc', 'docx', 'pdf', 'txt'].includes(fileExtension.toLowerCase())) {
-      category = FileCategory.Document;
-    } else {
-      category = FileCategory.Other;
-    }
+      if (['jpg', 'jpeg', 'png', 'gif', 'bmp'].includes(fileExtension.toLowerCase())) {
+        category = FileCategory.Image;
+      } else if (['doc', 'docx', 'pdf', 'txt'].includes(fileExtension.toLowerCase())) {
+        category = FileCategory.Document;
+      } else {
+        category = FileCategory.Other;
+      }
 
-    // Guardar los datos del archivo en la base de datos
-    const file = new this.fileModel({
-      filename,
-      originalname: uniqueFilename,
-      extension: fileExtension.toLowerCase(),
-      size: fileData.length,
-      filePath,
-      category,
-      status: 'active',
-    });
+      const file = new this.fileModel({
+        filename,
+        originalname: uniqueFilename,
+        extension: fileExtension.toLowerCase(),
+        size: fileData.length,
+        filePath,
+        category,
+        status: 'active',
+      });
 
-    await file.save();
+      await file.save();
 
-    return file
-    }
-    catch (error) {
+      this.logger.log(`Archivo guardado correctamente en ${filePath}`);
+      return file;
+    } catch (error) {
       this.logger.error(`Error al guardar el archivo: ${error.message}`);
       throw new Error('No se pudo guardar el archivo');
     }
